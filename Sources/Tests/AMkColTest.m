d9eb3888b751aa788927b0c0159fda6415ef3a7d
Sam Deane <sam@elegantchaos.com>
2013-04-23 11:18:52 +0100

    Improved test.

--- Sources/Tests/AMkColTest.m
+++ Sources/Tests/AMkColTest.m
@@ -11,14 +11,24 @@
 
 - (void)testRequest {
 
-    DAVMakeCollectionRequest *req = [self requestOfClass:[DAVMakeCollectionRequest class] withPath:@"davkittest"];
-    [self queueAndWaitForRequest:req];
-}
+    // delete the directory if it exists - we don't really care if this fails - it's just to attempt to clean up
+    DAVMakeCollectionRequest *deleteRequest = [self requestOfClass:[DAVDeleteRequest class] withPath:@"davkittest"];
+    [self queueAndWaitForRequest:deleteRequest];
 
-- (void)request:(DAVRequest *)aRequest didSucceedWithResult:(id)result {
-	STAssertNil(result, @"No result expected for MKCOL");
-	
-	[self notifyDone];
+    // try to make the directory
+    DAVMakeCollectionRequest *createRequest = [self requestOfClass:[DAVMakeCollectionRequest class] withPath:@"davkittest"];
+    [self queueAndWaitForRequest:createRequest];
+
+    // did we get an error?
+    STAssertNil(self.error, @"unexpected error for MKCOL %@", self.error);
+    STAssertNil(self.result, @"unexpected result for MKCOL %@", self.result);
+
+    // try to make the directory again - we should get back a 405, which we ignore
+    DAVMakeCollectionRequest *createRequest2 = [self requestOfClass:[DAVMakeCollectionRequest class] withPath:@"davkittest"];
+    [self queueAndWaitForRequest:createRequest2];
+
+    STAssertTrue(self.error.code == 405, @"unexpected error for MKCOL %@", self.error);
+    STAssertNil(self.result, @"unexpected result for MKCOL %@", self.result);
 }
 
 @end
