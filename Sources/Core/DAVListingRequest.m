f679fe0dc5ec15a40b3fd971a9540fc28bccf22c
vc <vinceplusplus@gmail.com>
2015-08-23 21:09:50 +0800

    removed unnecessary NSLog() from DAVListingRequest.m

--- Sources/Core/DAVListingRequest.m
+++ Sources/Core/DAVListingRequest.m
@@ -46,7 +46,6 @@
 
 - (id)resultForData:(NSData *)data {
     NSArray *items = nil;
-    NSLog(@"%@", [[[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding] autorelease]);
     if ([data length] > 0) {
         DAVListingParser *p = [[DAVListingParser alloc] initWithData:data];
 
